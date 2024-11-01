80729774a3f5e6f00850fb20047264e90d447575
Kurnia Muhamad <omenkzz@gmail.com>
2024-11-02 04:21:00 +0700

    create a reference of user on stripe

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  before_validation :create_stripe_reference, on: :create
+
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :stripe_id, presence: true, uniqueness: true
+
+  def create_stripe_reference
+    response = Stripe::Customer.create(email: email)
+    self.stripe_id = response.id
+  end
+
+  def retrieve_stripe_reference
+    Stripe::Customer.retrieve(stripe_id)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_10_23_084226) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_01_205718) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -22,6 +22,7 @@ ActiveRecord::Schema[8.0].define(version: 2024_10_23_084226) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "stripe_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
